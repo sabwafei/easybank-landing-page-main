247ea432b14dc2705d3080d380f08c77605e2a16
Christopher Smith <57920513+sabwafei@users.noreply.github.com>
2021-12-17 19:57:27 -0500

    nav menu mobile
    completed mobile nav menu
    toggle on/off
    also mobile styling with utility classes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // coded by SabWaFei
 //selectors
-let navLinkContainer = document.getElementById("nav-links-container");
+let mobileNavLinks = document.querySelector(".nav-links");
 let navButton = document.querySelector(".toggle");
-let exitButton = document.querySelector("fa-times");
+
+// toggles mobile nav menu on/off
 navButton.addEventListener("click", () => {
   if (navButton.classList.contains("fa-bars")) {
     //toggle icon
     navButton.classList.replace("fa-bars", "fa-times");
     //display nav menu
-    navLinkContainer.style.display = "block";
+    mobileNavLinks.style.display = "block";
   } else {
     navButton.classList.replace("fa-times", "fa-bars");
-    navLinkContainer.style.display = "none";
+    mobileNavLinks.style.display = "none";
   }
 });
